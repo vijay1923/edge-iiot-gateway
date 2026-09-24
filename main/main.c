e17983a6e4e790dcd03fe0040734032d31e53d05
vijay1923 <vijaymagadum1923@gmail.com>
2026-09-24 13:09:11 +0530

    test dummy indications passing app

--- main/main.c
+++ main/main.c
@@ -9,30 +9,29 @@
 #include "wifi.h"
 #include "mqtt.h"
 #include "ws2812.h"
+#include "indicator.h"
+
 
 
 static const char *TAG = "MAIN";
 
 static void app_task(void *pvParameters)
 {
-    int command = 0;
+    indicator_message_t message = {
+        .source = IND_SOURCE_SYSTEM,
+        .state = IND_STATE_STARTING,
+        .severity = IND_SEVERITY_INFO,
+        .activity = IND_ACTIVITY_NONE
+    };
+
+    indicator_report(message);
 
     while (1)
     {
-        ESP_LOGI(TAG, "Sending command: %d", command);
-
-        ws2812_send_command(command);
-
-        command++;
-
-        if (command > 2)
-        {
-            command = 0;
-        }
-
-        vTaskDelay(pdMS_TO_TICKS(500)); // 
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
+
 
 void app_main(void)
 {
@@ -43,6 +42,8 @@ void app_main(void)
     wifi_init();
 
     ws2812_init();
+
+    indicator_init();
 
     xTaskCreate(
         app_task,
